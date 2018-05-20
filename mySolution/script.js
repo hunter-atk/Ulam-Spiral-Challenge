f73318718ebfa576d75e111182bcae10d3ef0aff
Hunter Jefferson <hunterjefferson@Hunters-MBP.attlocal.net>
2018-05-20 11:39:04 -0500

    Nearly finished. Need addBorder function and need to modify createNumberSpiral to run no more than 144 times.

--- mySolution/script.js
+++ mySolution/script.js
@@ -11,47 +11,17 @@ const c = canvas.getContext('2d');
 
 
 //declare global variables
-let numberCount = 0;
-let instanceCount = 0;
-let xCoordinate = 0;
-let yCoordinate = 0;
+let numberCount = 1;
+let instanceCount = 1;
 
-
-//create spiral
-(function createNumberSpiral (x) {
-    const color = (numberCount / x == 0) ? 'green' : 'red';
-    c.fill.style = color;
-    c.fillRect(xCoordinate, yCoordinate, 50, 50);
-    instanceCount++;
-    numberCount++;
-    changeDirection();
-    //return final result
-})()
-
-
-//helper function to identify spiral 'scope'
 let round = 0;
 let waitTime = 1;
 let waitCount = 0;
 
-function evolveScope(){
-    if (instanceCount < 1){
-        return changeDirection()
-    }
-    waitCount++;
-    if (waitCount == waitTime) {
-        round++;
-        waitCount = 0;
-        changeDirection();
-    }
-    if (round == 2){
-        round = 0;
-        waitTime++;
-    }
-}
+let xCoordinate = 300;
+let yCoordinate = 300;
 
-
-//helper function to change spiral direction in 'scope'
+let directionCount = 1;
 const directions = {
     1: function () => {
         return xCoordinate += 50;
@@ -67,9 +37,60 @@ const directions = {
     }
 }
 
-let directionCount = 1; 
+
+//create spiral
+(function createNumberSpiral (x) {
+    const color = (((instanceCount-1) / x) == 0) ? 'green' : 'red';
+    c.fill.style = color;
+    c.fillRect(xCoordinate, yCoordinate, 50, 50);
+    evolveScope();
+    instanceCount++;
+    numberCount += x;
+})()
+
+
+//helper function to identify spiral 'scope'
+function evolveScope(){
+    if (instanceCount == 1){
+        return changeDirection()
+    }
+    waitCount++;
+    if (waitCount == waitTime) {
+        round++;
+        waitCount = 0;
+        changeDirection();
+    } else {
+        changeCoordinates();
+    }
+    if (round == 2){
+        round = 0;
+        waitTime++;
+    }
+}
+
+//helper function to change coordinates WITHOUT changing direction
+function changeCoordinates (){
+    switch(directionCount) {
+        case 1:
+            xCoordinate += 50;
+            break;
+        case 2:
+            yCoordinate += 50;
+            break;
+        case 3:
+            xCoordinate -= 50;
+            break;
+        case 4:
+            yCoordinate -= 50;
+        default:
+            console.log(err);
+    }
+}
+
+
+//helper function to change spiral direction in 'scope'
 function changeDirection () {
-    if(instanceCount > 0){
+    if(instanceCount > 1){
         directionCount++;
     }
     if(directionCount > 4){
@@ -77,8 +98,6 @@ function changeDirection () {
     }
     return directions.directionCount();
 }
-
-
 
 
 //helper function to add border to box
