1603e159d0aabad2d8155b37b36942bcf18d835f
Hunter Jefferson <hunterjefferson@Hunters-MBP.attlocal.net>
2018-05-20 07:43:29 -0500

    added comments for helper functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function createNumberSpiral (number){
+    //return final result
+}
+
+
+//helper function to identify spiral 'scope'
+
+
+//helper function to change spiral direction in 'scope'
+
+
+//helper function to add border to box
+function addBorder(direction){
+    //Add border to side of new box facing passed-in spiral direction
+}
